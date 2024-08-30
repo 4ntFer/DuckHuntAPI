c5ef545a5af1cb0afdb829f4e8e14cc149d2d602
Antonio Ferreira <antonio.ferreira@mps.com.br>
2024-08-30 08:15:19 -0300

    Correção

--- Repository/CharacterRepository.cs
+++ Repository/CharacterRepository.cs
@@ -19,9 +19,9 @@ namespace DuckHuntAPI.Models
         }
 
         public Character FindByName(string name) {
-            List<Character> list = session.Query<Character>().Where(c => c.name == name)().ToList();
-            if (list.Count != 0)
-                return list.ElementAt(0);
+            List<Character> Characterlist = session.Query<Character>().Where(c => c.name == name).ToList();
+            if (Characterlist.Count != 0)
+                return Characterlist.ElementAt(0);
             return null;
         }
     }
