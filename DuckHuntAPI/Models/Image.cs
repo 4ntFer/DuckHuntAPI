f202d967e6ed6cf364994a85cd54acd2fa34221d
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2024-12-19 13:49:06 -0300

    Re-mapeamento de todas as entidades para acertar os relacionamentos

--- DuckHuntAPI/Models/Image.cs
+++ DuckHuntAPI/Models/Image.cs
@@ -12,7 +12,6 @@ namespace DuckHuntAPI.Models
     {
         public virtual int id { get; set; }
         public virtual Byte[] data { get; set; }
-
         public virtual Character character { get; set; }
     }
 }
--- DuckHuntAPI/Models/ImageSeq.cs
+++ DuckHuntAPI/Models/ImageSeq.cs
@@ -10,24 +10,9 @@ namespace DuckHuntAPI.Models
     // </summary>
     public class ImageSeq
     {
-        public virtual int Id { get; set; }
-        public virtual int AnimationId { get; set; }
-        public virtual int ImageIndex { get; set; }
-        public virtual int ImageId { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            var t = obj as ImageSeq;
-            if (t == null)
-            {
-                return false;
-            }
-
-            return (AnimationId == t.AnimationId && ImageIndex == t.ImageIndex);
-        }
-        public override int GetHashCode()
-        {
-            return (AnimationId + "|" + ImageIndex).GetHashCode();
-        }
+        public virtual int id { get; set; }
+        public virtual int animationId { get; set; }
+        public virtual int imageIndex { get; set; }
+        public virtual Image image { get; set; }
     }
 }
