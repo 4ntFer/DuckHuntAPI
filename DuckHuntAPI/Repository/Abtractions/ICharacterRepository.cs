567a18d25fd3e7b0dc4a237378493f9a92dd3b9e
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2024-12-26 18:48:33 -0300

    Implemetação das abstrações de Repository, AnimationRepository, CharacterRepository e ImageRepository

---
+++ DuckHuntAPI/Repository/Abtractions/ICharacterRepository.cs
@@ -0,0 +1,14 @@
+﻿using DuckHuntAPI.Models;
+using NHibernate;
+
+namespace DuckHuntAPI.Repository.Abtractions
+{
+    public abstract class ICharacterRepository : IRepository<Character>
+    {
+        public ICharacterRepository(ISession session) : base(session)
+        {
+        }
+
+        public abstract Character FindByName(string name);
+    }
+}
---
+++ DuckHuntAPI/Repository/Abtractions/IImageRepository.cs
@@ -0,0 +1,17 @@
+﻿using DuckHuntAPI.Models;
+using NHibernate;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DuckHuntAPI.Repository.Abtractions
+{
+    public abstract class IImageRepository : IRepository<Image>
+    {
+        public IImageRepository(ISession session) : base(session)
+        {
+        }
+
+        public abstract IList<Image> OfAnimation(int AnimationId);
+        public abstract IList<Image> OfCharacter(int CharacterId);
+    }
+}
