46ddb5eab5b8674f7238e8e345a3dcee8594ac40
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2025-01-01 19:52:40 -0300

    Mapeamento da entidade CharacterImage

---
+++ DuckHuntAPI/Models/CharacterImage.cs
@@ -0,0 +1,9 @@
+﻿namespace DuckHuntAPI.Models
+{
+    public class CharacterImage
+    {
+        public virtual int id { get; set; }
+        public virtual Character character { get; set; }
+        public virtual Image image { get; set; }
+    }
+}
