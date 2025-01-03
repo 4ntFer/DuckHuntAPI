30a0fca538dfe336790eb4c260c816675d6a80a3
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2025-01-03 16:39:57 -0300

    Implementação de repository para a entidade Scenery

---
+++ DuckHuntAPI/Repository/Abtractions/ISceneryRepository.cs
@@ -0,0 +1,19 @@
+﻿using DuckHuntAPI.Models;
+using NHibernate;
+using Remotion.Linq.Clauses.ExpressionVisitors;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DuckHuntAPI.Repository.Abtractions
+{
+    public abstract class ISceneryRepository : IRepository<Scenery>
+    {
+        protected ISceneryRepository(ISession session) : base(session)
+        {
+        }
+
+        public abstract IList<Scenery> FindByName(string name);
+
+        public abstract IList<Scenery> FindByType(string type);
+    }
+}
