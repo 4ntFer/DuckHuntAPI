2889d64bb8bb8b4fa7b0ab71381dc18e7e55532b
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2025-01-03 17:33:42 -0300

    Criação de end points de cenários

---
+++ DuckHuntAPI/Controllers/SceneryController.cs
@@ -0,0 +1,77 @@
+﻿using DuckHuntAPI.DTO.Concrete;
+using DuckHuntAPI.Models;
+using DuckHuntAPI.Repository.Abtractions;
+using DuckHuntAPI.Repository.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DuckHuntAPI.Controllers
+{
+    [Route("api/scenery")]
+    public class SceneryController : ControllerBase
+    {
+        // Retorna todos os cenários
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAll() {
+            NHibernate.ISession session = NHibernateHelper.GetSession(HttpContext);
+            ISceneryRepository repository = new SceneryRepository(session);
+            IList<Scenery> sceneries = repository.FindAll();
+
+            return Ok(SceneryOutputDTO.CreateListOf(sceneries));
+        }
+
+        // Retorna os cenários por nome
+        [HttpGet]
+        [Route("byName/{name}")]
+        public IActionResult GetByName(string name) {
+            NHibernate.ISession session = NHibernateHelper.GetSession(HttpContext);
+            ISceneryRepository repository = new SceneryRepository(session);
+            IList<Scenery> sceneries = repository.FindByName(name);
+
+            return Ok(SceneryOutputDTO.CreateListOf(sceneries));
+        }
+
+        // Retorna os cenários por id
+        [HttpGet]
+        public IActionResult GetById(int id) {
+            NHibernate.ISession session = NHibernateHelper.GetSession(HttpContext);
+            ISceneryRepository repository = new SceneryRepository(session);
+            Scenery scenery = repository.FindById(id);
+
+            if(scenery == null)
+            {
+                return BadRequest($"Error: Scenery with id = {id} does not exist.");
+            }
+
+            return Ok(new SceneryOutputDTO(scenery));
+        }
+
+        // Retorna os cenários por tipo
+        [HttpGet]
+        public IActionResult GetByType(string type) {
+            NHibernate.ISession session = NHibernateHelper.GetSession(HttpContext);
+            ISceneryRepository repository = new SceneryRepository(session);
+            IList<Scenery> sceneries = repository.FindByType(type);
+
+            return Ok(SceneryOutputDTO.CreateListOf(sceneries));
+        }
+
+        // Retorna a imagem do cenário
+        [HttpGet]
+
+        public IActionResult GetImage(int id) {
+            NHibernate.ISession session = NHibernateHelper.GetSession(HttpContext);
+            ISceneryRepository repository = new SceneryRepository(session);
+            Scenery scenery = repository.FindById(id);
+
+            if (scenery == null)
+            {
+                return BadRequest($"Error: Scenery with id = {id} does not exist.");
+            }
+
+            return Ok(new SceneryOutputDTO(scenery).image);
+        }
+    }
+}
