13ce8e90b8871226db7616be999074117b12f939
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2025-01-03 16:29:28 -0300

    Mapeamento de entidade Scenery

---
+++ DuckHuntAPI/Models/Scenery.cs
@@ -0,0 +1,10 @@
+﻿namespace DuckHuntAPI.Models
+{
+    public class Scenery
+    {
+        public virtual int id { get; set; }
+        public virtual string name { get; set; }
+        public virtual string type { get; set; }
+        public virtual Image image { get; set; }
+    }
+}
