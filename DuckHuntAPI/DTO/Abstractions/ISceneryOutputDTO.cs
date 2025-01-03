9b5c4d987002d439372437fe856d52153c8cc529
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2025-01-03 17:13:27 -0300

    Criação de OutputDTO para entidade Scenery

---
+++ DuckHuntAPI/DTO/Abstractions/ISceneryOutputDTO.cs
@@ -0,0 +1,22 @@
+﻿using DuckHuntAPI.Models;
+using NHibernate.Mapping.ByCode;
+
+namespace DuckHuntAPI.DTO.Abstractions
+{
+    public abstract class ISceneryOutputDTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public IImageOutputDTO image { get; set; }
+
+        public ISceneryOutputDTO(Scenery scenery) {
+            id = scenery.id;
+            name = scenery.name;
+            type = scenery.type;
+            image = GetDTOImage(scenery.image);
+        }
+        protected abstract IImageOutputDTO GetDTOImage(Image image);
+        
+    }
+}
