cb150c4d747537ed6dd24de0029abedd834e510a
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2025-01-11 16:54:24 -0300

    Correção de mesmo cliente sendo banido várias vezes

--- DuckHuntAPI/Security/SecurityExecutor.cs
+++ DuckHuntAPI/Security/SecurityExecutor.cs
@@ -85,6 +85,14 @@ namespace DuckHuntAPI.Security
         {
             Device d = GetCurrentClientDevice();
 
+            Ban deviceBan = session.Query<Ban>()
+                   .Where(db => db.deviceId == d.id)
+                   .FirstOrDefault();
+
+            if (deviceBan == null) {
+                return false;
+            }
+
             if (d.accesses >= clientAllowedAccesses)
             {
                 return true;
